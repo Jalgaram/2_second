37a66817446c0afa63dc876af6fb89428c8a8243
Jalgaram <kgr424@naver.com>
2025-04-22 17:58:32 +0900

    2025-04-22

---
+++ js/member.js
@@ -0,0 +1,122 @@
+const shotaro = {
+    card_img: 'sub_photo/허그쇼타로.png',
+    hover_img: 'sub_photo/느좋쇼타로.png',
+    name: '大﨑 将太郎 (오사키 쇼타로, Osaki Shotaro)',
+    birth: '2000년 11월 25일 \n가나가와현 요코하마시',
+    nationality: '일본',
+    physical: '178cm | 62~63kg | A형 | 280mm',
+    family: '아버지 오사키 켄타로, 어머니, \n여동생 <span>(2005년생)</span>, 남동생 <span>(2012년생)</span>, 반려견 초코',
+    education: '대학교 (경제학/중퇴)',
+    agency: 'SM엔터테인먼트',
+    group: 'RIIZE',
+    position: '댄서, 래퍼',
+    career: 'NCT, NCT U <span>(2020.10.12 ~ 2023.05.24)</span>',
+    debut: '2020년 10월 12일 NCT U \n2023년 9월 4일 RIIZE',
+    nickname: '타로, 수달, 강아지, 수달강아지, 쇼, 타로짱(タロちゃん), 타로 버블티, 타로밀크티, 장태랑, 오타로, 광야 오씨, Fire Tornado Hector, 소달호, 수달호, 짱짱이, 쇼라에몽, Social Butterfly, 그사찍돼, 우팀쇼, 햄타로, 쇼뭉이, 쇼냥이, 쇼끼, 쇼카소',
+    mbti: 'ESFP',
+    emoji: '🧋',
+    sign_img: 'sub_photo/쇼타로싸인.png'
+}
+
+const eunseok = {
+    card_img: 'sub_photo/허그은석.png',
+    hover_img: 'sub_photo/느좋은석.png',
+    name: '송은석 (宋恩奭, Song Eun-seok)',
+    birth: '2001년 3월 19일 \n서울특별시 광진구 구의동',
+    nationality: '대한민국',
+    physical: '180cm | 59~60kg | AB형 | 260mm',
+    first_name: '은진 송 (恩津 宋氏)',
+    family: '아버지 송요한, 어머니, \n남동생 <span>(2015년생)</span>, \n반려견 찰리',
+    religion: '개신교',
+    education: '서울구남초등학교 <span>(졸업)</span> \n광진중학교 <span>(졸업)</span> \n광남고등학교 <span>(중퇴)</span>',
+    agency: 'SM엔터테인먼트',
+    group: 'RIIZE',
+    position: '보컬',
+    debut: '2023년 9월 4일 RIIZE',
+    nickname: '돌, 실버스톤, 송사리, 돌님, ㅅㅇㅅ, 송프, 돌민용, ok, 은서기, 잭송, SES, 블랙쉐도우, 은돌, 은석선배, 송동원, 구의동 민경훈, 송메치, 돌냥이',
+    mbti: 'ISTP',
+    emoji: '🪨',
+    sign_img: 'sub_photo/은석싸인.png'
+}
+
+const sungchan = {
+    card_img: 'sub_photo/허그성찬.png',
+    hover_img: 'sub_photo/느좋성찬.png',
+    name: '정성찬(鄭成燦, Jung Sungchan)',
+    birth: '2001년 9월 13일 \n서울특별시 강남구 개포동',
+    nationality: '대한민국',
+    physical: '185.6cm | 75kg | A형 | 270mm \n글씨만 왼속 사용',
+    family: '아버지 정석주, 어머니, \n형 <span>(1999년생)<span>, \n반려견 겨울',
+    education: '서울구룡초등학교 <span>(졸업)</span> \n언북중학교 <span>(졸업)</span> \n청담고등학교 <span>(중퇴)</span> \n고등학교 졸업 학력 검정고시 <span>(합격)</span>',
+    agency: 'SM엔터테인먼트',
+    group: 'RIIZE',
+    position: '래퍼',
+    career: 'NCT, NCT U <span>(2020.10.12 ~ 2023.05.24)<span>',
+    debut: '2020년 10월 12일 NCT U \n2023년 9월 4일 RIIZE',
+    nickname: '진수성찬, 밤비, 사슴, 왕눈 성찬, 치즈 걔, 디어 보이, 잼찬이, 아기 기린, 오렌지, 스포요정, 쿵쾅맨, 일렉트로닉 제피르, 말벌 아저씨, 숑블리치, 락밤',
+    mbti: 'ESTP',
+    emoji: '🐸',
+    sign_img: 'sub_photo/성찬싸인.png'
+}
+
+const wonbin = {
+    card_img: 'sub_photo/허그원빈.png',
+    hover_img: 'sub_photo/느좋원빈.png',
+    name: '박원빈 (朴元彬, Park Won-bin)',
+    birth: '2002년 3월 2일 서울특별시 성복구',
+    hometown: '울산광역시 울주군 범서읍 천상리',
+    nationality: '대한민국',
+    physical: '176cm | 55~57kg | AB형 | 270mm',
+    first_name: '은진 송 (恩津 宋氏)',
+    family: '아버지 박광훈, 어머니, 형 (2000년생)',
+    education: '으뜸과학유치원 (수료) 천상초등학교 (졸업) 울산스포츠과학중학교 (전학) 천상중학교 (졸업) 울산상업고등학교 (중퇴)',
+    agency: 'SM엔터테인먼트',
+    group: 'RIIZE',
+    position: '센터, 보컬, 댄서',
+    debut: '2023년 9월 4일 RIIZE',
+    nickname: '원반이, 깜고, 삐니, 락스타, 베이프남, 빅원빈, 빈욘세, 삔냥이, 울산 치타, 삔수종, 토냥덕, 어깨괴물, 암흑의 별 다크빈',
+    mbti: 'INTJ',
+    emoji: '🎸',
+    sign_img: 'sub_photo/원빈싸인.png'
+}
+
+const sohee = {
+    card_img: 'sub_photo/허그소희.png',
+    hover_img: 'sub_photo/느좋소희.png',
+    name: '이소희 (李炤煕, Lee Sohee)',
+    birth: '2003년 11월 21일 경기도 시흥시 능곡동',
+    nationality: '대한민국',
+    physical: '174cm | 54~55kg | A형 | 265mm',
+    family: '아버지 이형민, 어머니, 큰누나 (1999년생), 작은누나 이지민 (2001년생)',
+    education: '시흥능곡초등학교 (졸업) 시흥능곡중학교 (전학) 시흥능곡고등학교 (졸업)',
+    agency: 'SM엔터테인먼트',
+    group: 'RIIZE',
+    position: '메인 보컬',
+    debut: '2023년 9월 4일 RIIZE',
+    nickname: '또리, 끼끼, 이요이, 초딩, 어둠의 제왕 다크니스, 소루노 마스, 또냥이, 촌개, 소희주, 오리고기',
+    mbti: 'ISTP',
+    emoji: '💂',
+    sign_img: 'sub_photo/소희싸인.png'
+}
+
+const anton = {
+    card_img: 'sub_photo/허그앤톤.png',
+    hover_img: 'sub_photo/느좋앤톤.png',
+    name: 'Anton Chanyoung Lee (앤톤 찬영 리) 이찬영 (李燦榮, Lee Chanyoun)',
+    birth: ' 2004년 3월 21일 매사추세츠 주 보스턴',
+    nationality: '대한민국 | 미국 복수국적',
+    physical: '184cm | 65kg | O형 | 280mm',
+    first_name: '경주 이씨(慶州 李氏)',
+    family: '아버지 윤상, 어머니 심혜진, 남동생 이준영 (2009년생), 반려견 코코, 메이',
+    education: 'Dwight-Englewood School (중퇴) 고등학교 졸업 학력 검정고시 (합격) GED (합격 여부 미공개)',
+    agency: 'SM엔터테인먼트',
+    group: 'RIIZE',
+    position: '보컬',
+    debut: '2023년 9월 4일 RIIZE',
+    nickname: '브라키오, 윤찬영, 치유의 미카엘, 앤또니, 최애의 아이, itboy, 자이언트 베이비, 수호천사, 슈퍼샤이 보이, 멍톤이, 멍룡이, 차녕이',
+    mbti: 'INTP',
+    emoji: '🦕',
+    sign_img: 'sub_photo/앤톤싸인.png'
+}
+
+let members = [shotaro, eunseok, sungchan, wonbin, sohee, anton ]
